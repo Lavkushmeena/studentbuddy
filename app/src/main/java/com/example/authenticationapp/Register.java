a7e821df792ba398fa89d381f11e4dbd19b6e295
LAVKUSH <bt19cse006@nituk.ac.in>
2021-03-22 21:55:22 +0530

    login and register

---
+++ app/src/main/java/com/example/authenticationapp/Register.java
@@ -0,0 +1,24 @@
+package com.example.authenticationapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class Register extends AppCompatActivity {
+    EditText mFullName,mEmail,mPassword,mPhone;
+
+    Button mRegisterBtn;
+
+    TextView mLoginBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        mFullName = findViewById(R.id.fullName);
+        mEmail = findViewById(R.id.Email);
+    }
+}
